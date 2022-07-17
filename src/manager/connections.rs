b44e9c9974a9acadae62c6c5dc05ad011a2bc9f8
Taah <admin@taah.dev>
2022-07-17 02:54:13 -0700

    - Fix caching issues where the user would not store the player properly - A few poor changes, I'm not the best at Rust - Game Room & Player Caching - Game Data is next

--- src/manager/connections.rs
+++ src/manager/connections.rs
@@ -2,12 +2,25 @@ use crate::manager::user::User;
 use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::net::SocketAddr;
-use std::sync::Mutex;
 use tokio::net::UdpSocket;
+use tokio::sync::{Mutex, MutexGuard, OwnedMutexGuard};
+use tracing::log::info;
 
 lazy_static! {
     pub static ref CONNECTIONS: Mutex<HashMap<SocketAddr, Option<User>>> = {
         let mut m = HashMap::new();
         return Mutex::new(m);
     };
+}
+
+pub fn update_user(user: User) {
+    let user_clone = user.clone();
+    tokio::spawn(async move {
+        get_users().insert(user.socketAddr, Some(user_clone));
+        println!("updated");
+    });
+}
+
+pub fn get_users() -> MutexGuard<'static, HashMap<SocketAddr, Option<User>>> {
+    return futures::executor::block_on(CONNECTIONS.lock());
 }
